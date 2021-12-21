24a7ff0fe6e582a011db5f8138753e37f650dab8
Robin Horton <paklids@gmail.com>
2021-12-20 22:31:49 -0800

    refactor and README

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
-# WIP: shorthike
+# shorthike
 
 A portable golang utility for performing simple TCP checks and logging the results
 
-## Famous Last Words
+Famous last words --- "It's just a short hike...it won't take very long at all"
 
-"It's just a short hike, it won't take very long at all"
+Find out just how hard the journey will be before departing
 
-Find out just how hard the journey will be before departing.
+# How to use shorthike
+
+Best run inside a docker container. Check if your container or pod can connect to its dependency
+
+# Environment Variables
+
+TCPHEALTH_HOST_01 - either the hostname or ip address of the host (or service) you wish to connect to
+TCPHEALTH_PORT_01 - the TCP port on that host (or service)
+
+...you should be able to run as many number of checks as needed by incrementing the number above...
+
+TCPHEALTH_RUN_INTERVAL - how often you'd like the check performed, measured in golang time.ParseDuration()
+TCPHEALTH_CONNECT_TIMEOUT - how long the TCP connection attempt should try before failing
+
+LOG_LEVEL=DEBUG - debug logging turned on
--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 	"time"
 )
 
-//connect_timeout represented like 2s (2 seconds) or like 30ms (30 milliseconds)
-var connect_timeout, _ = time.ParseDuration(getEnv("TCPHEALTH_CONNECT_TIMEOUT", "1s"))
-
 //run_interval can be represented the same way - but never shorter than connect_interval
 var run_interval, _ = time.ParseDuration(getEnv("TCPHEALTH_RUN_INTERVAL", "10s"))
+
+//connect_timeout represented like 2s (2 seconds) or like 30ms (30 milliseconds)
+var connect_timeout, _ = time.ParseDuration(getEnv("TCPHEALTH_CONNECT_TIMEOUT", "1s"))
 
 func getEnv(key, fallback string) string {
 	if value, ok := os.LookupEnv(key); ok {
@@ -26,6 +26,7 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Connection timeout is too high\n")
 		os.Exit(1)
 	}
+	fmt.Println()
 	dispatcherTicker := time.NewTicker(time.Duration(run_interval))
 	for {
 		select {
@@ -36,19 +37,15 @@ func main() {
 }
 
 func dispatcher() {
-	// set the env var for testing
-	//os.Setenv("TCPHEALTH_HOST_01", "www.google.com")
-	//os.Setenv("TCPHEALTH_PORT_01", "80")
-
-	fmt.Println()
 	for _, e := range os.Environ() {
 		pair := strings.SplitN(e, "=", 2)
 		if strings.Contains(pair[0], "TCPHEALTH_HOST_") {
-			//fmt.Println(pair[0] + "=" + pair[1])
 			ID := strings.SplitN(pair[0], "_", 3)
 			PORT := os.Getenv("TCPHEALTH_PORT_" + ID[2])
 			addr := checkIPAddress(pair[1])
-			//fmt.Println("-- Attempting connection to ", pair[1], " on ", PORT, " --")
+			if "DEBUG" == getEnv("LOG_LEVEL", "WARN") {
+				fmt.Println("-- Attempting connection to ", pair[1], " on ", PORT, " --")
+			}
 			raw_connect(pair[1], addr, PORT)
 		}
 	}
@@ -56,12 +53,16 @@ func dispatcher() {
 
 func checkIPAddress(ip string) (addr string) {
 	if net.ParseIP(ip) == nil {
-		//fmt.Printf("Host: %s - is NOT an IP address\n", ip)
+		if "DEBUG" == getEnv("LOG_LEVEL", "WARN") {
+			fmt.Printf("Host: %s - is NOT an IP address\n", ip)
+		}
 		addr, err := net.LookupIP(ip)
 		if err != nil {
 			fmt.Println("Host", ip, "does not resolve properly")
 		} else {
-			//fmt.Println("Host "+ip+" resolves to: ", addr)
+			if "DEBUG" == getEnv("LOG_LEVEL", "WARN") {
+				fmt.Println("Host "+ip+" resolves to: ", addr)
+			}
 			return addr[0].String()
 		}
 	} else {
@@ -72,7 +73,9 @@ func checkIPAddress(ip string) (addr string) {
 }
 
 func raw_connect(host string, addr string, port string) {
-	fmt.Println("Connection timeout is ", connect_timeout)
+	if "DEBUG" == getEnv("LOG_LEVEL", "WARN") {
+		fmt.Println("Connection timeout is", connect_timeout)
+	}
 	timeout := time.Duration(connect_timeout)
 	conn, err := net.DialTimeout("tcp", net.JoinHostPort(addr, port), timeout)
 	if err != nil {
